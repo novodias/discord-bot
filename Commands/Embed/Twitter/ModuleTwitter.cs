7bfed9fd13a1e1993f1b812dae002f15bd28f838
novodias <nil.diasbessa@gmail.com>
2022-01-07 19:18:14 -0300

    Added Twitter API, removed old command

--- Commands/Embed/Twitter/ModuleTwitter.cs
+++ Commands/Embed/Twitter/ModuleTwitter.cs
@@ -10,134 +10,11 @@ namespace DiscordBot.Commands.Embed.Twitter
 {
     public class ModuleTwitter : BaseCommandModule
     {
-        [Command("twitteruser")]
-        public async Task TwitterCommand(CommandContext ctx, [RemainingText] string strUser)
-        {
-            string path = $"files/data/guilds/{ctx.Guild.Id}";
-
-            try
-            {
-                string twtUrl = $"https://twitter.com/{strUser}";
-                
-                using ( var client = new HttpClient() )
-                {
-                    using ( var response = await client.GetAsync(twtUrl) )
-                    {
-                        response.EnsureSuccessStatusCode();
-
-                        var doc = await response.Content.ReadAsStringAsync();
-                        //response.Dispose();
-
-                        File.WriteAllText($"{path}/twitter.html", doc);
-
-                        var html = new HtmlDocument();
-                        html.Load($"{path}/twitter.html");
-
-                        try
-                        {
-                            var profileavatar = html.DocumentNode.SelectSingleNode("//div[@class='ProfileAvatar']//a//img").GetAttributeValue("src", "");
-                            var screename = html.DocumentNode.SelectSingleNode("//head/title").InnerText;
-                            var atname = $"@{strUser}";
-                            screename = screename.Remove(screename.IndexOf(" ("));
-                            var tweet = html.DocumentNode.SelectSingleNode("//div[@class='js-tweet-text-container']//p").InnerText;
-
-                            if (html.DocumentNode.SelectSingleNode("//div[contains(@class,'AdaptiveMedia-video')]") != null)
-                            {
-                                var media = html.DocumentNode.SelectSingleNode("//div[contains(@class,'PlayableMedia-player')]").GetAttributes("style", "");
-                                var mediaUrl = media.First().Value;
-                                mediaUrl = mediaUrl.Substring(mediaUrl.IndexOf("https://"));
-
-                                // mediaUrl refuses to get the last index.
-                                mediaUrl = mediaUrl.Remove(mediaUrl.LastIndexOf(@".jpg"));
-                                mediaUrl += ".jpg";
-
-                                var thumbnail = new DiscordEmbedBuilder.EmbedThumbnail()
-                                {
-                                    Height = 48,
-                                    Width = 48,
-                                    Url = profileavatar
-                                };
-
-                                var embed = new DiscordEmbedBuilder()
-                                {
-                                    Title = atname,
-                                    Description = tweet,
-                                    Thumbnail = thumbnail,
-                                    ImageUrl = mediaUrl,
-                                    Url = $"https://www.twitter.com/{strUser}"
-                                };
-
-                                var msg = await new DiscordMessageBuilder()
-                                    .WithEmbed(embed)
-                                    .WithReply(ctx.Member.Id, true)
-                                    .SendAsync(ctx.Channel);   
-                            }
-                            else if (html.DocumentNode.SelectSingleNode("//div[contains(@class,'AdaptiveMedia-singlePhoto')]") != null)
-                            {
-
-                                var media = html.DocumentNode.SelectSingleNode("//div[contains(@class,'AdaptiveMedia-photoContainer js-adaptive-photo')]").GetAttributeValue("data-image-url", "");
-                                
-                                var thumbnail = new DiscordEmbedBuilder.EmbedThumbnail()
-                                {
-                                    Height = 48,
-                                    Width = 48,
-                                    Url = profileavatar
-                                };
-
-                                var embed = new DiscordEmbedBuilder()
-                                {
-                                    Title = atname,
-                                    Description = tweet,
-                                    Thumbnail = thumbnail,
-                                    ImageUrl = media,
-                                    Url = $"https://www.twitter.com/{strUser}"
-                                };
-
-                                var msg = await new DiscordMessageBuilder()
-                                    .WithEmbed(embed)
-                                    .WithReply(ctx.Member.Id, true)
-                                    .SendAsync(ctx.Channel);
-                            }
-                            else
-                            {
-                                var thumbnail = new DiscordEmbedBuilder.EmbedThumbnail()
-                                {
-                                    Height = 48,
-                                    Width = 48,
-                                    Url = profileavatar
-                                };
-
-                                var embed = new DiscordEmbedBuilder()
-                                {
-                                    Title = atname,
-                                    Description = tweet,
-                                    Thumbnail = thumbnail,
-                                    Url = $"https://www.twitter.com/{strUser}"
-                                };
-
-                                var msg = await new DiscordMessageBuilder()
-                                    .WithEmbed(embed)
-                                    .WithReply(ctx.Member.Id, true)
-                                    .SendAsync(ctx.Channel);
-                            }
-                        }
-                        catch (Exception ex)
-                        {
-                            File.WriteAllText("files/dump.txt", ex.StackTrace);   
-                        }
-
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                await ctx.RespondAsync("Usuario nao encontrado");
-            }
-        }
-
         [Command("twitter")]
         public async Task FetchUserCommand(CommandContext ctx, [RemainingText] string strUser)
         {
+            await ctx.TriggerTypingAsync();
+
             string[] keys = File.ReadAllLines("files/twitterkey.txt");
 
             string message = string.Empty;
@@ -177,21 +54,28 @@ namespace DiscordBot.Commands.Embed.Twitter
                 {
                     if (timeline.FirstOrDefault().Media.FirstOrDefault() != null)
                     {
-                        var thumbnail = new DiscordEmbedBuilder.EmbedThumbnail()
+                        var author = new DiscordEmbedBuilder.EmbedAuthor()
                         {
-                            Height = 48,
-                            Width = 48,
-                            Url = user.User.ProfileImageUrl
+                            IconUrl = user.User.ProfileImageUrl,
+                            Name = $"@{strUser}",
+                            Url = $"https://www.twitter.com/{strUser}"
+                        };
+
+                        var footer = new DiscordEmbedBuilder.EmbedFooter()
+                        {
+                            Text = "amogus",
+                            IconUrl = "https://i.imgur.com/82HZ341.png"
                         };
 
                         var embed = new DiscordEmbedBuilder()
                         {
                             Title = strUser,
                             Description = timeline.First().Text,
-                            Thumbnail = thumbnail,
-                            Color = DiscordColor.Blue,
-                            ImageUrl = timeline.First().Media.First().MediaURL,
-                            Url = $"https://www.twitter.com/{strUser}"
+                            Color = DiscordColor.Aquamarine,
+                            ImageUrl = timeline.First().Media.First().MediaURLHttps,
+                            Timestamp = timeline.First().CreatedAt,
+                            Author = author,
+                            Footer = footer
                         };
 
                         var msg = await new DiscordMessageBuilder()
@@ -201,20 +85,27 @@ namespace DiscordBot.Commands.Embed.Twitter
                     }
                     else
                     {
-                        var thumbnail = new DiscordEmbedBuilder.EmbedThumbnail()
+                        var author = new DiscordEmbedBuilder.EmbedAuthor()
                         {
-                            Height = 48,
-                            Width = 48,
-                            Url = user.User.ProfileImageUrl
+                            IconUrl = user.User.ProfileImageUrl,
+                            Name = $"@{strUser}",
+                            Url = $"https://www.twitter.com/{strUser}"
+                        };
+
+                        var footer = new DiscordEmbedBuilder.EmbedFooter()
+                        {
+                            Text = "amogus",
+                            IconUrl = "https://i.imgur.com/82HZ341.png"
                         };
 
                         var embed = new DiscordEmbedBuilder()
                         {
-                            Title = strUser,
+                            Title = user.User.Name,
                             Description = timeline.First().Text,
-                            Thumbnail = thumbnail,
-                            Color = DiscordColor.Blue,
-                            Url = $"https://www.twitter.com/{strUser}"
+                            Color = DiscordColor.Aquamarine,
+                            Timestamp = timeline.First().CreatedAt,
+                            Author = author,
+                            Footer = footer
                         };
 
                         var msg = await new DiscordMessageBuilder()
